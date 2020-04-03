937e879f03fbfcfe61acadebca0f914de1f6e7d8
Girish-21706 <32380394+Girish-21706@users.noreply.github.com>
2020-04-03 13:59:00 +0530

    file12.cpp
    simple way to reading from file

---
+++ read.cpp
@@ -0,0 +1,23 @@
+/* C++ File Handling - Retrieve information from the file in C++ */
+		
+#include<iostream.h>
+#include<conio.h>
+#include<fstream.h>
+void main()
+{
+	ifstream fin;
+	char fname[20];
+	char rec[80];
+	clrscr();
+
+	cout<<"Enter file name: ";
+	cin>>fname;
+
+	cout<<"The file contains:\n";
+	fin.open(fname, ios::in);
+	fin.get(rec, 80);
+	cout<<rec;
+
+	fin.close();
+	getch();
+}
