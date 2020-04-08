573e410f5063c5ce085b6fdd68b2bac5468768ad
Girish-21706 <32380394+Girish-21706@users.noreply.github.com>
2020-04-08 11:17:27 +0530

    commandline2.cpp
    command line program simple example

---
+++ commandline2.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main(int argc, char* argv[])
+{
+clrscr();
+  cout<<"\n Program name  : \n"<<argv[0];
+  cout<<"1st arg :\n"<<argv[1];
+  cout<<"2nd arg :\n"<<argv[2];
+  cout<<"3rd arg :\n"<<argv[3];
+  cout<<"4th arg :\n"<<argv[4];
+  cout<<"5th arg :\n"<<argv[5];
+getch();
+}
